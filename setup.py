9e69459f35f1beca7913f0bf9711f7a3070406e8
Vijay Kumar <vijaykumar@bravegnu.org>
2015-01-15 23:45:37 +0530

    Bump up the version no. to 0.2.0.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='gallows',
-      version='0.1.0',
+      version='0.2.0',
       description=("The word game Hangman based on 'Invent Your "
                    "Own Computer Games with Python'."),
       author='Vijay Kumar B.',
